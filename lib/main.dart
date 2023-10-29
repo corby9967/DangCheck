7887c61049caba92b6b69266eb86319de41e539b
corby9967 <corby9967@gmail.com>
2023-10-30 01:21:54 +0900

    Wade

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:dangcheck/pages/auth.dart';
+import 'package:dangcheck/pages/chat.dart';
 import 'package:dangcheck/pages/home.dart';
-import 'package:dangcheck/pages/house.dart';
-import 'package:dangcheck/pages/make_house_pages/make_house.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
           primary: const Color(0xFFFF961A),
         ),
       ),
-      home: const HousePage(),
+      home: const HomePage(newCode: ''),
     );
   }
 }

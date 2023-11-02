b917f0dc327fd8370bb6be63d42ea46e64ade011
corby9967 <corby9967@gmail.com>
2023-11-03 01:13:23 +0900

    + splash page

---
+++ lib/splash.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:dangcheck/pages/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:get/get.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Timer(
+      const Duration(seconds: 3),
+      () => Get.to(const AuthPage(), transition: Transition.zoom),
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            const Image(image: AssetImage('assets/images/splash.png')),
+            Positioned(
+                top: 50,
+                left: MediaQuery.of(context).size.width / 2 - 30,
+                child: const SpinKitThreeInOut(
+                  size: 30,
+                  color: Colors.white,
+                  delay: Duration(milliseconds: 20),
+                  duration: Duration(milliseconds: 400),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
